d33bc47bbf9e734eea880ea07f6da0ec3030a379
Steel Wheels Project <wheel@tcn.zaq.ne.jp>
2025-02-26 23:09:13 +0900

    project: initial commit

---
+++ TerminalKit/TerminalKit_iOS/TerminalKit_iOS.h
@@ -0,0 +1,18 @@
+//
+//  TerminalKit_iOS.h
+//  TerminalKit_iOS
+//
+//  Created by Tomoo Hamada on 2025/02/26.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for TerminalKit_iOS.
+FOUNDATION_EXPORT double TerminalKit_iOSVersionNumber;
+
+//! Project version string for TerminalKit_iOS.
+FOUNDATION_EXPORT const unsigned char TerminalKit_iOSVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <TerminalKit_iOS/PublicHeader.h>
+
+
---
+++ TerminalKit/TerminalKit_macOS/TerminalKit.h
@@ -0,0 +1,18 @@
+//
+//  TerminalKit.h
+//  TerminalKit
+//
+//  Created by Tomoo Hamada on 2025/02/26.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for TerminalKit.
+FOUNDATION_EXPORT double TerminalKitVersionNumber;
+
+//! Project version string for TerminalKit.
+FOUNDATION_EXPORT const unsigned char TerminalKitVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <TerminalKit/PublicHeader.h>
+
+
