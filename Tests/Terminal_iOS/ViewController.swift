0249501ae15ec69654787072c2d26b5a9e876590
Steel Wheels Project <wheel@tcn.zaq.ne.jp>
2025-03-23 15:15:28 +0900

    test: Add unit test for iOS: Terminal_iOS

---
+++ Tests/Terminal_iOS/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Terminal_iOS
+//
+//  Created by Tomoo Hamada on 2025/03/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+        override func viewDidLoad() {
+                super.viewDidLoad()
+                // Do any additional setup after loading the view.
+        }
+
+
+}
+
