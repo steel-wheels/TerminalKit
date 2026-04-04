42a6bc0c04fac5416388d43c16d6d60f9acc6727
Steel Wheels Project <wheel@tcn.zaq.ne.jp>
2026-04-04 20:11:32 +0900

    UnitTest: remove shell

--- Tests/UnitTest_macOS/ViewController.swift
+++ Tests/UnitTest_macOS/ViewController.swift
@@ -14,13 +14,23 @@ import Cocoa
 class ViewController: NSViewController
 {
         @IBOutlet var mTerminalView: MITerminalView!
-        private   var mPipeInterfgace  = MIPipeInterface()
-        private   var mShell: KSShell? = nil
+        private   var mTerminalToShellPipe:     Pipe?    = nil
+        private   var mShellToTerminalPipe:     Pipe?    = nil
+        private   var mErrorPile:               Pipe?    = nil
 
         override func viewDidLoad() {
                 super.viewDidLoad()
 
-                mTerminalView.fileInterface = MIFileInterface(asSlave: mPipeInterfgace)
+                let terminalToShellPipe = Pipe()
+                let shellToTerminalPipe = Pipe()
+                let errorPipe           = Pipe()
+                mTerminalToShellPipe    = terminalToShellPipe
+                mShellToTerminalPipe    = shellToTerminalPipe
+                mErrorPile              = errorPipe
+
+                mTerminalView.standardInput  = shellToTerminalPipe.fileHandleForReading
+                mTerminalView.standardOutput = terminalToShellPipe.fileHandleForWriting
+                mTerminalView.standardError  = errorPipe.fileHandleForWriting
 
                 // Do any additional setup after loading the view.
                 let textcol = MIColor.green
@@ -28,16 +38,30 @@ class ViewController: NSViewController
                 mTerminalView.textColor       = textcol
                 mTerminalView.backgroundColor = backcol
 
-                // store message
+                // connect standart output
+                terminalToShellPipe.fileHandleForReading.setReader(reader: {
+                        (_ str: String) -> Void in
+                        switch MIEscapeCode.decode(string: str) {
+                        case .success(let ecodes):
+                                for ecode in ecodes {
+                                        NSLog("[stdout] \(ecode.description())")
+                                }
+                        case .failure(let err):
+                                let msg = MIError.toString(error: err)
+                                NSLog("[output error] \(msg)")
+                        }
+                })
+                // connect standard error
+                errorPipe.fileHandleForWriting.setReader(reader: {
+                        (_ str: String) -> Void in NSLog("[stderr] \(str)")
+                })
+                // store initial message
                 let commands: Array<MITextEditCommand> = [
                         .setTextColor(textcol),
                         .setBackgroundColor(backcol),
                         .insertText(" ")
                 ]
                 mTerminalView.execute(commands: commands)
-
-                let shell = KSShell(fileInterface: MIFileInterface(asMaster: mPipeInterfgace))
-                mShell = shell
         }
 
         override var representedObject: Any? {
